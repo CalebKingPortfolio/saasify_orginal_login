fdc66412ed757f8cff82409ee48d97bf5ff54062
CalebKingPortfolio <calebking9876@gmail.com>
2025-06-24 08:00:27 +1200

    unit_testing_added

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+import unittest
+from app import app
+
+class SaaSifyLoginTests(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_redirect(self):
+        response = self.client.get('/login')
+        self.assertEqual(response.status_code, 302)  # Redirect to Google
+
+    def test_protected_area_redirect(self):
+        response = self.client.get('/protected_area')
+        self.assertEqual(response.status_code, 302)  # Should redirect if not logged in
+
+if __name__ == '__main__':
+    unittest.main()
