18bfa173e51e5f781068e1fdea2e6d8f7a73a9b5
CalebKingPortfolio <calebking9876@gmail.com>
2025-06-24 06:32:41 +1200

    First Commit

---
+++ app.py
@@ -0,0 +1,71 @@
+import os
+import pathlib
+import requests
+from flask import Flask, render_template, session, redirect, request, abort
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
+app = Flask("SaaSify Google Login")
+app.secret_key = "esc70wegQpJ9jTBia4eWEdpqy9r49cAU"
+
+GOOGLE_CLIENT_ID = "362632682751-mn02ajnfa960bh48d20e3afb3rke00ea.apps.googleusercontent.com"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=[
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "https://www.googleapis.com/auth/userinfo.email",
+        "openid"
+    ],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if session.get("state") != request.args.get("state"):
+        return abort(400)
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["user_email"] = id_info.get("email")
+    session["user_name"] = id_info.get("name")
+    return redirect("/protected_area")
+
+@app.route("/protected_area")
+def protected_area():
+    if "user_email" not in session:
+        return redirect("/")
+    return render_template("protected_area.html", user_name=session["user_name"])
+
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    session.clear()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
